1a2eb74f881169e9d223d136aeda2357b14e4600
shiva-69 <92380211+shiva-69@users.noreply.github.com>
2022-04-06 00:42:12 +0530

    Progress in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,35 +3,85 @@ import styled from 'styled-components';
 
 
 const Navbar_wrapper = styled.div`
-background-color: #cecece;
+// background-color: #cecece;
 position : sticky;
 margin-left : 14px;
 margin-right : 14px;
-margin-top: 2px;
+margin-top: 1%;
 margin-bottom: 2px;
 height: 50px;
 display: flex;
 flex-direction: row;
+font-family: "Segoe UI", Arial, sans-serif;
 `;
 
 const Logo_wrapper= styled.div`
 margin-right: 10px;
 border-radius: 50px;
-flex-grow : 1;
+width: auto;
 padding: auto;
+margin: auto 1%;
 `
 
 const Home_div = styled.div`
-flex-grow : 1;
-background-color: yellow;
+width: auto;
+margin: auto 1%;
+font-weight: bold;
+font-size :120%;
 `
 const Today_div = styled.div`
-flex-grow : 1;
-background-color: orange;
+width: auto;
+margin: auto 5px;
+font-weight: bold;
+font-size :120%;
 `
 const Search_div = styled.div`
-flex-grow : 5;
-background-color: yellow;
+width: 72%;
+color: rgb(118,118,118);
+margin: auto 1%;
+border-radius : 50px;
+// background-color: red;
+height: 100%;
+
+&:hover{
+  background-color: #E1E1E1;
+}
+`
+const Notification = styled.div`
+margin: auto 0px;
+`
+const Message = styled.div`
+margin: auto 0px;
+`
+const Profile_Pic = styled.div`
+padding : auto 5px;
+margin: auto 0px;
+`
+const Drop_Down = styled.div`
+margin: auto 0px;
+// padding-left: 4%
+`
+const Input= styled.input`
+height: 100%;
+width : 96%;
+color : #333;
+margin: 0px;
+padding: auto ;
+outline : none;
+border: none;
+font-size :100%;
+// margin-bottom: 100px;
+border-radius : 50px;
+
+&:hover{
+  background-color: #E1E1E1;
+}
+`
+const Svg = styled.svg`
+margin:0px;
+margin: auto 10px;
+// margin-top: 2%;
+paddin-top: 4%
 `
 
 
@@ -51,11 +101,34 @@ export const Navbar = () => {
         <Today_div>
           <span>
             Today
-          </span>
+            </span>
+          
         </Today_div>
         <Search_div>
-          Search
+        <Svg class="gUZ B9u U9O kVc" height="16" width="16" viewBox="0 0 24 24" aria-label="Search icon" role="img" fill = "rgb(118,118,118)"><path d="M10 16c-3.31 0-6-2.69-6-6s2.69-6 6-6 6 2.69 6 6-2.69 6-6 6m13.12 2.88-4.26-4.26A9.842 9.842 0 0 0 20 10c0-5.52-4.48-10-10-10S0 4.48 0 10s4.48 10 10 10c1.67 0 3.24-.41 4.62-1.14l4.26 4.26a3 3 0 0 0 4.24 0 3 3 0 0 0 0-4.24"></path></Svg>
+          <Input type="text" placeholder='Search'/>
         </Search_div>
+
+        <Notification>
+        <Svg class="gUZ B9u U9O kVc" height="24" width="24" viewBox="0 0 24 24" aria-hidden="true" aria-label="" role="img" fill = "rgb(118,118,118)" ><path d="M12 24c-1.66 0-3-1.34-3-3h6c0 1.66-1.34 3-3 3zm7-10.83c1.58 1.52 2.67 3.55 3 5.83H2c.33-2.28 1.42-4.31 3-5.83V7c0-3.87 3.13-7 7-7s7 3.13 7 7v6.17z"></path></Svg>
+        </Notification>
+
+
+        <Message>
+        <Svg class="Hn_ gUZ B9u U9O kVc" height="24" width="24" viewBox="0 0 24 24" aria-hidden="true" aria-label="" role="img" fill = "rgb(118,118,118)"><path d="M18 12.5a1.5 1.5 0 1 1 .001-3.001A1.5 1.5 0 0 1 18 12.5m-6 0a1.5 1.5 0 1 1 .001-3.001A1.5 1.5 0 0 1 12 12.5m-6 0a1.5 1.5 0 1 1 .001-3.001A1.5 1.5 0 0 1 6 12.5M12 0C5.925 0 1 4.925 1 11c0 2.653.94 5.086 2.504 6.986L2 24l5.336-3.049A10.93 10.93 0 0 0 12 22c6.075 0 11-4.925 11-11S18.075 0 12 0"></path></Svg>
+        </Message>
+
+        
+
+        <Profile_Pic>
+        <Svg class="Hn_ gUZ B9u U9O kVc" height="24" width="24" viewBox="0 0 24 24" aria-hidden="true" aria-label="" role="img" fill = "rgb(118,118,118)"><path d="M18 12.5a1.5 1.5 0 1 1 .001-3.001A1.5 1.5 0 0 1 18 12.5m-6 0a1.5 1.5 0 1 1 .001-3.001A1.5 1.5 0 0 1 12 12.5m-6 0a1.5 1.5 0 1 1 .001-3.001A1.5 1.5 0 0 1 6 12.5M12 0C5.925 0 1 4.925 1 11c0 2.653.94 5.086 2.504 6.986L2 24l5.336-3.049A10.93 10.93 0 0 0 12 22c6.075 0 11-4.925 11-11S18.075 0 12 0"></path></Svg>
+        </Profile_Pic>
+
+
+
+        <Drop_Down>
+        <Svg class="gUZ B9u U9O kVc" height="12" width="12" viewBox="0 0 24 24" aria-hidden="true" aria-label="" role="img" fill = "rgb(118,118,118)"><path d="M12 19.5.66 8.29c-.88-.86-.88-2.27 0-3.14.88-.87 2.3-.87 3.18 0L12 13.21l8.16-8.06c.88-.87 2.3-.87 3.18 0 .88.87.88 2.28 0 3.14L12 19.5z"></path></Svg>
+        </Drop_Down>
     </Navbar_wrapper>
   )
 }
